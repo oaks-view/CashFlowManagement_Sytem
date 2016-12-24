72382af5c188a744c29bbd9fead1b6722e66d642
moses igbuku <moses igbuku>
2016-12-24 15:42:06 +0100

    liltle change

---
+++ CashFlowManagement.Core/Services/EntitySort.cs
@@ -0,0 +1,24 @@
+﻿using CashFlowManagement.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlowManagement.Core.Services
+{
+    public class EntitySort
+    {
+        public static List<Income> SortedList(List<Income> incomeObjects)
+        {
+            List<Income> incomes = incomeObjects.OrderByDescending(i => i.DateCreated).ToList();
+            return incomes;
+        }
+
+        public static List<Expense> SortedList(List<Expense> incomeObjects)
+        {
+            List<Expense> expenses = incomeObjects.OrderByDescending(i => i.DateCreated).ToList();
+            return expenses;
+        }
+    }
+}
--- CashFlowManagement.Core/Services/ExpenseService.cs
+++ CashFlowManagement.Core/Services/ExpenseService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CashFlowManagement.Core.Services.EntitySort;
 
 namespace CashFlowManagement.Core.Services
 {
